e39a659c0b269218075268ad819b8652e85ab43c
Hemlocked <phantomofrotten@gmail.com>
2018-12-27 17:22:21 +0900

    リクエストによって違う表示

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var http = require('http');
 var number = require('./settings');
 var server = http.createServer();
 server.on ('request', function(req, res) {
+	switch (req.url) {
+		case '/about':
+		    msg = "about this page";
+		    break;
+        case '/profile':
+            msg = "about me";
+            break;
+        default:
+            msg = 'wrong page';
+            break;
+	}
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world');
+    res.write(msg);
     res.end();
 });
 server.listen(number.port, number.host);
