23c5c650aff00137c9b442a265ecb6bf01ea889f
Hemlocked <phantomofrotten@gmail.com>
2018-12-28 17:40:42 +0900

    POST送信が可能

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
+'use strict';
+
 var http = require('http');
 var fs = require('fs');
-var number = require('./settings');
+var setting = require('./settings');
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/hello.ejs', 'utf-8');
+var template = fs.readFileSync(__dirname + '/bbs.ejs', 'utf-8');
 var ejs = require('ejs');
-var n = 0;
+var qs = require('querystring');
+var posts = [];
+
+function renderForm(posts, res) {
+	var data = ejs.render(template, {
+		posts: posts
+	});
+		res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+}
 
 server.on ('request', function(req, res) {
-    n++;
-    var data = ejs.render(template, {
-        title: "hello",
-        content: "<strong>World!</strong>",
-        n: n
-    });
-	res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+	if (req.method === 'POST') {
+		req.data = "";
+		req.on("readable", function() {
+			req.data += req.read();
+		});
+		req.on("end", function() {
+            var query = qs.parse(req.data);
+            posts.push(query.name);
+            renderForm(posts, res);
+		});
+	} else {
+		renderForm(posts, res);
+	}
 });
     
 server.listen(number.port, number.host);
