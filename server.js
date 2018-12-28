554d70a662a08e4932ef798789e3dc37c0b488ff
Hemlocked <phantomofrotten@gmail.com>
2018-12-28 13:38:11 +0900

    ファイルを表示

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 var number = require('./settings');
 var server = http.createServer();
+
 server.on ('request', function(req, res) {
-	switch (req.url) {
-		case '/about':
-		    msg = "about this page";
-		    break;
-        case '/profile':
-            msg = "about me";
-            break;
-        default:
-            msg = 'wrong page';
-            break;
-	}
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
+    fs.readFile(__dirname + '/hello.html', 'utf-8', function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write("not found!");
+            return res.end();
+        }
+	res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
     res.end();
+	});
 });
+    
 server.listen(number.port, number.host);
 console.log("server listening ...");
